b7e05c3102df49a1def813bbf69f33f062241aee
s950ms20 <39444613+s950ms20@users.noreply.github.com>
2023-03-21 23:45:38 +0100

    Menu Btn fixed

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -16,9 +16,9 @@ import {
   animations: [
     trigger('rotatedState', [
       state('default', style({ transform: 'rotate(0deg)' })),
-      state('rotated', style({ transform: 'rotate(180deg)' })),
-      transition('rotated => default', animate('500ms ease-out')),
-      transition('default => rotated', animate('500ms ease-in')),
+      state('rotated', style({ transform: 'rotate(90deg)' })),
+      transition('rotated => default', animate('200ms')),
+      transition('default => rotated', animate('200ms')),
     ]),
   ],
 })
