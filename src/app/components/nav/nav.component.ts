de8ab4def1d8adea8b4694bd487578d3acdb5412
s950ms20 <39444613+s950ms20@users.noreply.github.com>
2023-03-22 23:24:18 +0100

    'test'

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -17,8 +17,8 @@ import {
     trigger('rotatedState', [
       state('default', style({ transform: 'rotate(0deg)' })),
       state('rotated', style({ transform: 'rotate(90deg)' })),
-      transition('rotated => default', animate('1s')),
-      transition('default => rotated', animate('1s')),
+      transition('rotated => default', animate('500ms')),
+      transition('default => rotated', animate('500ms')),
     ]),
   ],
 })
